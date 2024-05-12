40ec8217239b9efe13d4ec9aec91430023fa08f7
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2024-05-11 21:36:50 -0400

    ChooChooTrain, Pompon Plant, TransparentWall, and all other new ProductMapObjDataTable classes

---
+++ modules/ProductMapObjDataTableClasses/include/ProductMapObjDataTableClasses.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "syati.h"
+
+class AirFar100m;
+class AssemblyBlock;
+class BallOpener;
+class BeeJumpBall;
+class GoroRockLaneParts;
+class GravityFallBlock;
+class InvisiblePolygonObj;
+class JumpHole;
+class ManholeCover;
+class RailMoveTimerObj;
+class RepeatTimerSwitchingBlock;
+class Shellfish;
+class SimpleEnvironmentObj;
+class SimpleTextureSwitchChangeObj;
+class WoodBox;
