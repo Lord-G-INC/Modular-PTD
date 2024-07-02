b73c6dafc4903716ba00af236fc5eceff7506ee7
nico-apps <nicomarikucza@googlemail.com>
2024-07-02 12:56:39 +0200

    Remove unnecessary comment

--- PTD/BenefitItemMoon/include/BenefitItemMoon.h
+++ PTD/BenefitItemMoon/include/BenefitItemMoon.h
@@ -2,17 +2,15 @@
 
 #include "syati.h"
 #include "revolution.h"
-//#include "Game/LiveActor/FlashingCtrl.h"
     
-    class BenefitItemMoon : public LiveActor {
-    public:
-        BenefitItemMoon(const char* pName);
-        virtual void init(const JMapInfoIter& rIter);
-        virtual void control();
-        virtual bool receiveMessage(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
-    
-        void collect();
-        void appearAndMove();
+class BenefitItemMoon : public LiveActor {
+public:
+    BenefitItemMoon(const char* pName);
+    virtual void init(const JMapInfoIter& rIter);
+    virtual void control();
+    virtual bool receiveMessage(u32 msg, HitSensor* pSender, HitSensor* pReceiver);
+    void collect();
+    void appearAndMove();
 
-        bool mIsCollected;
-    };
+    bool mIsCollected;
+};
