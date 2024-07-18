15bd9c232dd2be05b4eb941646d8893024cb40f0
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2024-07-18 09:13:04 -0400

    Update SMSS code

--- ExtraModules/Starshine/source/Starshine.cpp
+++ ExtraModules/Starshine/source/Starshine.cpp
@@ -9,7 +9,6 @@ kmWrite32(0x80464300, 0x7FC3F378); // mr r3, r30
 kmCall(0x80464304, changeCoinCounterAppearanceSpot);
 kmWrite32(0x80464308, 0x48000024); // b 0x24
 
-// Always attach CoinCounter to the "Position0001" pane 
-//instead of picking from multiple based off of coin amount
-kmWrite32(0x80464630, 0x48000070); // b 0x70
-kmWrite32(0x804646A4, 0x48000008); // b 8
+// Detach the Coin Counter from Position panes
+kmWrite32(0x80464630, 0x48000088); // b 0x88
+kmWrite32(0x804642DC, 0x48000010); // b 0x10
