e4d10275e0a53c43af0f9eb7a7e933ef34feedc8
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2024-10-20 16:24:28 -0400

    ExtStationedFileTable description and TWN support

--- PTD/ExtStationedFileTable/source/ExtStationedFileTable.cpp
+++ PTD/ExtStationedFileTable/source/ExtStationedFileTable.cpp
@@ -1,5 +1,11 @@
 #include "ModuleData_ExtStationedFileTable.h"
 
+#if defined TWN || defined KOR
+    #define REGIONOFF 0x90
+#else
+    #define REGIONOFF 0
+#endif
+
 asm void createAndAddNewStationed() {
     stwu r1, -0x10(r1)
     mflr r0
@@ -9,19 +15,18 @@ asm void createAndAddNewStationed() {
 
     lwz r28, 4(r30) 
     lwz r29, 8(r30)
-
+    
     cmpwi r29, 0
     bne end
     cmpwi r28, 9
     beq end
+
     lis r3, cNewStationedFileEntries@ha
     addi r3, r3, cNewStationedFileEntries@l
     addi r30, r3, 0xC
-    lwz r28, 4(r30)
-    lwz r29, 8(r30)
 
     end:
-    mr r3, r29
+    lwz r3, 8(r30)
     lwz r0, 0x14(r1)
     lwz r28, 0x10(r1)
     lwz r29, 0xC(r1)
@@ -30,8 +35,8 @@ asm void createAndAddNewStationed() {
     blr
 }
 
-kmWrite32(0x804CDF44, 0x2C030000);
-kmCall(0x804CDF40, createAndAddNewStationed);
+kmWrite32(0x804CDF40 + REGIONOFF, 0x2C030000);
+kmCall(0x804CDF44 + REGIONOFF, createAndAddNewStationed);
 
 
 asm void loadNewResources() {
@@ -43,19 +48,18 @@ asm void loadNewResources() {
 
     lwz r28, 4(r31) 
     lwz r29, 8(r31)
-
+    
     cmpwi r29, 0
     bne end
     cmpwi r28, 9
     beq end
+
     lis r3, cNewStationedFileEntries@ha
     addi r3, r3, cNewStationedFileEntries@l
     addi r31, r3, 0xC
-    lwz r28, 4(r31)
-    lwz r29, 8(r31)
 
     end:
-    mr r3, r29
+    lwz r3, 8(r31)
     lwz r0, 0x14(r1)
     lwz r28, 0x10(r1)
     lwz r29, 0xC(r1)
@@ -64,8 +68,8 @@ asm void loadNewResources() {
     blr
 }
 
-kmWrite32(0x804CDE68, 0x2C030000);
-kmCall(0x804CDE6C, loadNewResources);
+kmWrite32(0x804CDE68 + REGIONOFF, 0x2C030000);
+kmCall(0x804CDE6C + REGIONOFF, loadNewResources);
 
-kmWrite32(0x804CDD14, 0x2C030000);
-kmCall(0x804CDD18, loadNewResources);
+kmWrite32(0x804CDD14 + REGIONOFF, 0x2C030000);
+kmCall(0x804CDD18 + REGIONOFF, loadNewResources);
