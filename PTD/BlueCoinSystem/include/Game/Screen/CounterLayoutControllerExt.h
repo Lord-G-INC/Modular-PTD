d3b915da1fc6bc8636feaca6217411bd33107816
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2025-03-02 15:08:42 -0500

    BlueCoinSystem and CustomCoinSpawns are now elsewhere

--- PTD/BlueCoinSystem/include/Game/Screen/CounterLayoutControllerExt.h
+++
@@ -1,9 +0,0 @@
-#include "Game/Screen/CounterLayoutController.h"
-
-class BlueCoinCounter;
-class CounterLayoutControllerExt : public CounterLayoutController {
-public:
-    CounterLayoutControllerExt();
-    s32* mGlobalLayoutHolder;
-    BlueCoinCounter* mBlueCoinCounter;
-};
