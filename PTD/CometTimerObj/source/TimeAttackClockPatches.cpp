946bc0f7ef1b66ec9704c58d156dbf7f1b00b66f
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2025-08-12 18:50:57 -0400

    TimeAttackClockPatches update

--- PTD/CometTimerObj/source/TimeAttackClockPatches.cpp
+++ PTD/CometTimerObj/source/TimeAttackClockPatches.cpp
@@ -15,6 +15,14 @@ TimeAttackClockExt* createTimeAttackClockExt(const char* pName) {
 kmCall(0x8033FFC4, createTimeAttackClockExt); // bl createTimeAttackClockExt
 kmWrite32(0x8033FFC8, 0x48000014); // b 0x14
 
+void TimeAttackClockInit(TimeAttackClockExt* pActor, const JMapInfoIter& rIter, const char* pStr, bool b) {
+    MR::processInitFunction(pActor, rIter, pStr, b);
+    MR::getJMapInfoArg0NoInit(rIter, (s32*)&pActor->mAddTime);
+    OSReport("%d\n", pActor->mAddTime);
+}
+
+kmCall(0x8032326C, TimeAttackClockInit);
+
 void TimeAttackClockAddTimer(TimeAttackClockExt* pClock, const char* pStr) {
     MR::startActionSound(pClock, pStr, -1, -1, -1);
 
