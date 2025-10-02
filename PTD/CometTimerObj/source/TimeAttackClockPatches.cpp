81f5ec8c47568131f421a1bb0c1fb16a45e2e620
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2025-10-02 19:19:20 -0400

    Syati_ClassExtensions + TimeAttackClockPatches change

--- PTD/CometTimerObj/source/TimeAttackClockPatches.cpp
+++ PTD/CometTimerObj/source/TimeAttackClockPatches.cpp
@@ -17,7 +17,7 @@ kmWrite32(0x8033FFC8, 0x48000014); // b 0x14
 
 void TimeAttackClockInit(TimeAttackClockExt* pActor, const JMapInfoIter& rIter, const char* pStr, bool b) {
     MR::processInitFunction(pActor, rIter, pStr, b);
-    MR::getJMapInfoArg0NoInit(rIter, (s32*)&pActor->mAddTime);
+    MR::getJMapInfoArg0NoInit(rIter, &pActor->mAddTime);
 }
 
 kmCall(0x8032326C, TimeAttackClockInit);
